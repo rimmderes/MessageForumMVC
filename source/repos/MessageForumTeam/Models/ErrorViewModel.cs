bbdb04ca6017bf18e55593a715546728dfde1717
Rimm Deres <rimmderes@gmail.com>
2023-05-03 10:46:18 +0100

    first commit

---
+++ source/repos/MessageForumTeam/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MessageForumTeam.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
