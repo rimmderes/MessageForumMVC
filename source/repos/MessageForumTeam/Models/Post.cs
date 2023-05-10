4463f8e2aa399f35b8c30890458ad33fbbd249ab
Connel Nwaodor <connel_n@hotmail.com>
2023-05-10 15:58:00 +0100

    Added user class and skeleten user methods

--- source/repos/MessageForumTeam/Models/Post.cs
+++ source/repos/MessageForumTeam/Models/Post.cs
@@ -1,5 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.RegularExpressions;
 
 namespace MessageForumTeam.Models
 {
@@ -7,43 +8,99 @@ namespace MessageForumTeam.Models
     {
         public Guid Id { get; set; }
 
-        [StringLength(50, MinimumLength = 3)]
+        [StringLength(20, MinimumLength = 3)]
         [Required]
         public string? Title { get; set; }
         public string? Description { get; set; }
+
         [Display(Name = "User")]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9]*$")]
         public string? Author { get; set; }
         [Display(Name = "Posted on")]
         [DataType(DataType.Date)]
         public DateTime DatePosted { get; set; }
 
         [StringLength(200, MinimumLength = 1)]
-        public string? Comments { get; set; }    
+        public string? Comments { get; set; }
         public string? Likes { get; set; }
         public string? Tags { get; set; }
 
 
+        //method to cap on screen title length
 
-        public string MaxTitleLength()
+        public string MaxDescriptionLength()
         {
-            //int length = this.Title.Length;
-            int maxLength = 20;
-            string newTitle = this.Title.Length > maxLength ? this.Title.Substring(0, maxLength) + "..." : this.Title;
-            return newTitle;
+            int maxLength = 50;
+            string newDesc = this.Description.Length > maxLength ? this.Description.Substring(0, maxLength) + "..." : this.Description;
+            return newDesc;
 
         }
 
+        // method to show the 
         public int LikesToInt()
         {
             if (this.Likes == null)
             {
                 return 0;
-            } else
+            }
+            else
             {
-            string[] LikesArr = this.Likes.Split(" ");
+                string[] LikesArr = this.Likes.Split(" ");
                 return LikesArr.Length;
             }
-            
+        }
+
+        public string[] CommentsToArray()
+        {
+            string[] commentsArr = Array.Empty<string>();
+            if (this.Comments == null)
+            {
+                return commentsArr;
+            }
+            else
+            {
+                commentsArr = (this.Comments.Split(" "));
+                return commentsArr;
+            }
+        }
+
+        public int CommentsToInt()
+        {
+            return CommentsToArray().Length;
+        }
+
+
+
+        //Methods to add and remove 'likes'
+        public bool CheckLike(User user)
+        {
+
+            if (this.Likes != null && this.Likes.Contains(user.username))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public void AddLike(User user)
+        {
+            if (this.Likes != null && !CheckLike(user))
+            {
+                this.Likes.Concat(user.username + " ");
+
+            }
+        }
+
+        public void RemoveLike(User user)
+        {
+
+            if (this.Likes != null && CheckLike(user))
+            {
+                int start = this.Likes.IndexOf(user.username), end = this.Likes.IndexOf(user.username) + user.username.Length;
+                this.Likes.Remove(start, end);
+            }
+
         }
     }
+
 }
---
+++ source/repos/MessageForumTeam/Models/User.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MessageForumTeam.Models
+{
+    public class User
+
+    {
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9]*$")]
+        [Required]
+        public string? username { get; set; }
+        public string? password { get; set; }
+        public string? email { get; set; }
+        public Guid Id { get; set; }
+
+        public User(string username, string password, string email)
+        {
+            this.email = email;
+            this.username = username;
+            this.password = password;
+        }
+
+        public void AddComment(Post post, string comment)
+        {
+            post.Comments.Concat(" " + comment);
+        }
+
+    }
+}
